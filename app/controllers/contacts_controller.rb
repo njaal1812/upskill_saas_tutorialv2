e51cbc21bda9f226f1e3f2e1670b2cdd84d93245
Njaal Kværnes <njaal1812@me.com>
2018-01-04 21:56:10 +0000

    Added styles to flash

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,16 +2,18 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-    def create
+  
+  def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      flash[:success] = "Message sent."
-       redirect_to new_contact_path
+     flash[:success] = "Message sent."
+     redirect_to new_contact_path
     else
-      flash[:error] = @contact.errors.full_messages.join(", ")
-      redirect_to new_contact_path
+     flash[:error] = @contact.errors.full_messages.join(", ")
+     redirect_to new_contact_path
     end
-    end
+  end
+    
     private
     def contact_params
        params.require(:contact).permit(:name, :email, :comments)
